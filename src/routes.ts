7bd5cb375808a92311e7c05ff006c8a58d504d56
Gabriel Sampaio <gasampaio.souza@gmail.com>
2021-06-11 19:20:45 -0300

    change heroku settings

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,10 @@ import StoresController from './controllers/stores_controller';
 const ROUTER = express.Router();
 var CORS_OPTIONS = getCorsOptions();
 
+ROUTER.get('/', (_, res) => {
+  res.json({ success: true })
+});
+
 ROUTER.get('/md/:store/:acronym', cors(CORS_OPTIONS), StoresController.find);
 ROUTER.post('/md/:store/:acronym', cors(CORS_OPTIONS), StoresController.create);
 ROUTER.patch('/md/:store/:acronym', cors(CORS_OPTIONS), StoresController.update);
